e5dc0d73d34c5caf167ef4af55d7128615c95cc1
yyune <kathyy0919@gmail.com>
2025-06-01 06:35:57 -0700

    latest manga should work

--- js/main.js
+++ js/main.js
@@ -10,33 +10,34 @@ $('.navbar-light .navbar-toggler').click(() => {
 
 /* end navbar */
 
+document.addEventListener('DOMContentLoaded', () => {
 fetch('data/manga.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById("mangaContainer");
+    container.innerHTML = ''; // clear container first
+
     data.forEach(manga => {
-      container.innerHTML += `
-        <div class="col-lg-2 col-md-3 col-sm-4">
-          <div class="card mb-3">
-            <a href="details.html?manga=${manga.folder}">
+      // encode the folder and chapterFile for URL
+      const filePath = encodeURIComponent(`${manga.folder}/${manga.chapterFile}`);
+
+      const html = `
+        <div class="col-lg-2 col-md-3 col-sm-4 mb-4">
+          <div class="card">
+            <a href="read.html?file=${filePath}">
               <img src="${manga.img}" class="card-img-top" alt="${manga.title}">
             </a>
-            <div class="over text-center">
-              <div class="head text-left">
-                <h6>${manga.title}</h6>
-              </div>
-              <div class="about-list">
-                <table class="table table-borderless">
-                  <tbody>
-                    <tr><th scope="row">Update:</th><td>${manga.latestVol}</td></tr>
-                  </tbody>
-                </table>
-              </div>
-              <p class="about text-muted">${manga.desc}</p>
-              <a class="reading btn" href="details.html?manga=${manga.folder}">Start Reading</a>
+            <div class="card-body">
+              <h5 class="card-title"><a href="read.html?file=${filePath}">${manga.title}</a></h5>
+              <p class="card-text">${manga.desc}</p>
+              <small>Latest: ${manga.latestVol}</small>
             </div>
           </div>
         </div>
       `;
+
+      container.insertAdjacentHTML('beforeend', html);
     });
-  });
+  })
+  .catch(err => console.error('Failed to load manga data:', err));
+    });
